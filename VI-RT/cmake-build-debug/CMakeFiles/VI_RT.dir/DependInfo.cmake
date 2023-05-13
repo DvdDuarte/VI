1f2d19d83131033aa25123131c97201b0eab11c9
Francisco Peixoto <thtr.irubra@gmail.com>
2023-05-13 18:59:37 +0100

    gitignore

--- VI-RT/cmake-build-debug/CMakeFiles/VI_RT.dir/DependInfo.cmake
+++ VI-RT/cmake-build-debug/CMakeFiles/VI_RT.dir/DependInfo.cmake
@@ -11,10 +11,13 @@ set(CMAKE_DEPENDS_DEPENDENCY_FILES
   "E:/GIT/VI/VI-RT/VI-RT/Camera/perspective.cpp" "CMakeFiles/VI_RT.dir/VI-RT/Camera/perspective.cpp.obj" "gcc" "CMakeFiles/VI_RT.dir/VI-RT/Camera/perspective.cpp.obj.d"
   "E:/GIT/VI/VI-RT/VI-RT/Image/ImagePPM.cpp" "CMakeFiles/VI_RT.dir/VI-RT/Image/ImagePPM.cpp.obj" "gcc" "CMakeFiles/VI_RT.dir/VI-RT/Image/ImagePPM.cpp.obj.d"
   "E:/GIT/VI/VI-RT/VI-RT/Primitive/BRDF/Phong.cpp" "CMakeFiles/VI_RT.dir/VI-RT/Primitive/BRDF/Phong.cpp.obj" "gcc" "CMakeFiles/VI_RT.dir/VI-RT/Primitive/BRDF/Phong.cpp.obj.d"
+  "E:/GIT/VI/VI-RT/VI-RT/Primitive/Geometry/Triangle.cpp" "CMakeFiles/VI_RT.dir/VI-RT/Primitive/Geometry/Triangle.cpp.obj" "gcc" "CMakeFiles/VI_RT.dir/VI-RT/Primitive/Geometry/Triangle.cpp.obj.d"
   "E:/GIT/VI/VI-RT/VI-RT/Primitive/Geometry/mesh.cpp" "CMakeFiles/VI_RT.dir/VI-RT/Primitive/Geometry/mesh.cpp.obj" "gcc" "CMakeFiles/VI_RT.dir/VI-RT/Primitive/Geometry/mesh.cpp.obj.d"
   "E:/GIT/VI/VI-RT/VI-RT/Renderer/StandardRenderer.cpp" "CMakeFiles/VI_RT.dir/VI-RT/Renderer/StandardRenderer.cpp.obj" "gcc" "CMakeFiles/VI_RT.dir/VI-RT/Renderer/StandardRenderer.cpp.obj.d"
   "E:/GIT/VI/VI-RT/VI-RT/Scene/scene.cpp" "CMakeFiles/VI_RT.dir/VI-RT/Scene/scene.cpp.obj" "gcc" "CMakeFiles/VI_RT.dir/VI-RT/Scene/scene.cpp.obj.d"
   "E:/GIT/VI/VI-RT/VI-RT/Shader/AmbientShader.cpp" "CMakeFiles/VI_RT.dir/VI-RT/Shader/AmbientShader.cpp.obj" "gcc" "CMakeFiles/VI_RT.dir/VI-RT/Shader/AmbientShader.cpp.obj.d"
+  "E:/GIT/VI/VI-RT/VI-RT/Shader/DistributedShader.cpp" "CMakeFiles/VI_RT.dir/VI-RT/Shader/DistributedShader.cpp.obj" "gcc" "CMakeFiles/VI_RT.dir/VI-RT/Shader/DistributedShader.cpp.obj.d"
+  "E:/GIT/VI/VI-RT/VI-RT/Shader/WhittedShader.cpp" "CMakeFiles/VI_RT.dir/VI-RT/Shader/WhittedShader.cpp.obj" "gcc" "CMakeFiles/VI_RT.dir/VI-RT/Shader/WhittedShader.cpp.obj.d"
   "E:/GIT/VI/VI-RT/VI-RT/main.cpp" "CMakeFiles/VI_RT.dir/VI-RT/main.cpp.obj" "gcc" "CMakeFiles/VI_RT.dir/VI-RT/main.cpp.obj.d"
   "E:/GIT/VI/VI-RT/VI-RT/utils/AABB.cpp" "CMakeFiles/VI_RT.dir/VI-RT/utils/AABB.cpp.obj" "gcc" "CMakeFiles/VI_RT.dir/VI-RT/utils/AABB.cpp.obj.d"
   )
